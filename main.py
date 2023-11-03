cbf26795204b999169322478b9099a165472e8ae
EmilTsaturyan <146758789+EmilTsaturyan@users.noreply.github.com>
2023-11-04 00:27:54 +0400

    v1

--- main.py
+++ main.py
@@ -2,15 +2,44 @@ import telebot
 from removebg import RemoveBackGround
 from PIL import Image
 
-bot = telebot.TeleBot("BOT TOKEN")
+bot = telebot.TeleBot("6871923653:AAHNxCmaxf6sbNc9lJi3mHK_R2DIZY7CZAY")
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "Hello🖐️, this bot can remove background from your image just sent it.")
+    bot.send_message(message.chat.id, """🌟"" Welcome to the Background Remover Bot! 🌟
+
+I'm here to help you easily remove the background from your images. Simply send me a photo, and I'll work my magic! ✨
+
+Here's how it works:
+1. Send me a photo with a clear subject you want to keep.
+2. Wait a moment while I process the image.
+3. Voilà! I'll send you back the image with the background removed.
+
+Please keep in mind:
+- For best results, use high-resolution images with a distinct subject.
+- I don't store or share any of your images.
+
+Feel free to give it a try! Send me an image and let's get started. If you have any questions, just type 'help'.
+
+Enjoy using the Background Remover Bot! 📸✨
+""")
 
 @bot.message_handler(commands=['help'])
 def help(message):
-    bot.send_message(message.chat.id, 'Send the image and bot will remove the background of the image')
+    bot.send_message(message.chat.id, """🤖 **Background Remover Bot Help** 🤖
+
+Need some assistance? No problem! Here are some commands you can use:
+
+- **/help**: Display this help message.
+- **/start**: Start using the bot.
+
+Remember these tips for the best results:
+- Use high-resolution images.
+- Make sure the subject is clear and distinct from the background.
+
+If you have any other questions or need further assistance, feel free to ask!
+
+Happy background removing! 📸✨""")
 
 @bot.message_handler(content_types=['photo'])
 def get_broadcast_picture(message):
